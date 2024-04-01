a8b2d66cb33029fd26f5bacaa61903f97a8bd4fb
Cllouth <126470179+Cllouth@users.noreply.github.com>
2024-04-01 11:37:53 -0300

    Add files via upload

---
+++ BD_Verictus.sql
@@ -0,0 +1,15 @@
+create database verictus;
+use verictus;
+
+create table usuarios_verictus(
+	id int primary key auto_increment,
+    nome varchar(255),
+    email varchar(255) not null,
+    senha varchar(255) not null
+);
+create table historico(
+	id_pesquisa int primary key auto_increment,
+	data_pesquisa datetime not null,
+    link_site varchar(255) not null,
+    resposta varchar(255)
+);
